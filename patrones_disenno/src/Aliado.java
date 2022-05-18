c17d240d1aa33cc5296fc60125ff8cd2bf9568bd
Erick <efmz2000@outlook.es>
2022-05-18 16:40:49 -0600

    aliado corregido

--- patrones_disenno/src/Aliado.java
+++ patrones_disenno/src/Aliado.java
@@ -7,7 +7,7 @@ import java.util.*;
  */
 public class Aliado extends Entidades {
 
-    public Aliado(int pos_x, int pos_y, Color enemyColor, int vida) {
+    public Aliado(int pos_x, int pos_y) {
         super(pos_x, pos_y, Ally_Color, 1);
         //TODO Auto-generated constructor stub
     }
